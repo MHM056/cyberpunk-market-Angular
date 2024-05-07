cf6ae2c010a2b795063f3713890f16220501279c
MHM056 <mihailmarholev@gmail.com>
2024-05-07 17:57:46 +0300

    Created home component and added routes for it
    Added HTML and CSS for home component

--- cyberpunk-market/src/app/app.module.ts
+++ cyberpunk-market/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
