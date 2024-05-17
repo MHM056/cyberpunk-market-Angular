0c244e6185ee95e9dd5a9a4aa04e7dfdcc20e6a5
MHM056 <mihailmarholev@gmail.com>
2024-05-17 21:46:41 +0300

    Created loader spinner component
    Added HTML and CSS for loader component;
    Implemented loader component in register component for testing purposes;

--- cyberpunk-market/src/app/shared/shared.module.ts
+++ cyberpunk-market/src/app/shared/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailDirective } from './validators/email.directive';
 import { NotificationComponent } from './notification/notification.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 
 @NgModule({
   declarations: [
     EmailDirective,
-    NotificationComponent
+    NotificationComponent,
+    LoaderComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [EmailDirective, NotificationComponent]
+  exports: [EmailDirective, NotificationComponent, LoaderComponent]
 })
 export class SharedModule { }
