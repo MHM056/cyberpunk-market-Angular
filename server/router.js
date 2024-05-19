ec6f425750b93ff5bce6ef31de2362d85f1e99b9
MHM056 <mihailmarholev@gmail.com>
2024-05-19 20:42:50 +0300

    Created marketController.js and added it to router.js

--- server/router.js
+++ server/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userController = require('./controllers/userController');
+const marketController = require('./controllers/marketController');
 
 router.use('/users', userController);
+router.use('/market', marketController);
 
 module.exports = router;
