92e57072dc15cf6b92c4b33c68f6cd097d839469
MHM056 <mihailmarholev@gmail.com>
2024-05-19 21:54:07 +0300

    Updated market controller
    Added get functionality for items;

--- server/controllers/marketController.js
+++ server/controllers/marketController.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+const marketService = require('../services/marketService');
 
 router.get('/items', async (req, res) => {
+    const items = await marketService.getAll().lean();
+
+    res.status(200).json(items);
 });
 
 
