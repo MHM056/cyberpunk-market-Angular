4279fc7a655ad89972fc1f31b45c778765ed58d6
MHM056 <mihailmarholev@gmail.com>
2024-05-19 21:53:34 +0300

    Created item type

---
+++ cyberpunk-market/src/app/types/item.ts
@@ -0,0 +1,13 @@
+export interface Item {
+    _id: string;
+    item: string;
+    imageUrl: string;
+    price: number;
+    availability: string;
+    type: string;
+    description: string;
+    userId: string;
+    created_at: string;
+    updatedAt: string;
+    _v: number;
+}
