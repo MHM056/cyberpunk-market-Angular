83be0f7a7c14912f4d5e033a65c70d58e333e365
MHM056 <mihailmarholev@gmail.com>
2024-05-22 17:50:32 +0300

    Updated marketController.js
    added post request functionality;

--- server/controllers/marketController.js
+++ server/controllers/marketController.js
@@ -7,5 +7,14 @@ router.get('/items', async (req, res) => {
     res.status(200).json(items);
 });
 
+router.post('/items', async (req, res) => {
+    const itemData = req.body;
+    try {
+        await marketService.create(itemData);
+        res.status(200).send('Successfully posted a new item!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
 
 module.exports = router;
