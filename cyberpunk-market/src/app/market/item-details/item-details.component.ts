de3b66f39a9f8228f32929e3214519a1f8ff29ac
MHM056 <mihailmarholev@gmail.com>
2024-05-24 19:34:27 +0300

    Updated item-details component
    Edit and Delete buttons are only visible for the item's creator

--- cyberpunk-market/src/app/market/item-details/item-details.component.ts
+++ cyberpunk-market/src/app/market/item-details/item-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MarketService } from '../market.service';
 import { Item } from 'src/app/types/item';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-item-details',
@@ -9,10 +10,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./item-details.component.css']
 })
 export class ItemDetailsComponent implements OnInit {
-  constructor(private marketService: MarketService, private route: ActivatedRoute) {}
+  constructor(private marketService: MarketService, private userService: UserService, private route: ActivatedRoute) {}
 
   item = {} as Item;
   itemId: string = '';
+
+  get userId(): string {
+    return this.userService.user?._id || '';
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => this.itemId = params['itemId']);
