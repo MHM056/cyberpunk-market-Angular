e03c2fa1603b4feabb58e691b71ff8494095df13
MHM056 <mihailmarholev@gmail.com>
2024-06-01 20:43:37 +0300

    Initialized profile component
    Created static HTML and CSS for the component:
    Added paths for the component;

--- cyberpunk-market/src/app/user/user.module.ts
+++ cyberpunk-market/src/app/user/user.module.ts
@@ -6,11 +6,13 @@ import { UserRoutingModule } from './user-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule, SharedModule, FormsModule, UserRoutingModule, RouterModule, ReactiveFormsModule
